bb63e4c1b07d0a08f63fdf38cc692142a3c41e3e
Primero1800 <Primero@inbox.ru>
2024-12-26 09:47:33 +0300

    settings edited

--- mine_shop/mine_shop/settings.py
+++ mine_shop/mine_shop/settings.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'mine_shop.urls'
